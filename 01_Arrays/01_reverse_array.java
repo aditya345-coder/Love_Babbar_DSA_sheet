a47f4fa32e0a888f4ca6bfa2d6f2b150cf05c70b
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2022-10-27 01:17:15 +0530

    updated 01_reverse_array.java

--- 01_Arrays/01_reverse_array.java
+++ 01_Arrays/01_reverse_array.java
@@ -1,1 +1,19 @@
+/* link : https://www.codingninjas.com/codestudio/problems/reverse-the-array_1262298?topList=love-babbar-dsa-sheet-problems&leftPanelTab=1
+*/
 
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution 
+{
+    public static void reverseArray(ArrayList<Integer> arr, int m)
+    {
+          int n = arr.size();
+          for (int i = m+1;i<n;i++)
+          {
+              Collections.swap(arr,i,n-1);
+              n--;
+          }
+    }
+}
