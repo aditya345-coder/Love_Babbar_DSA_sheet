cc460b6ce1b0d75f88394d6118c1a2475dce2dd2
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2022-10-28 22:12:28 +0530

    Create 04_Sort_0_1_2.java

---
+++ 01_Arrays/04_Sort_0_1_2.java
@@ -0,0 +1,12 @@
+/* Link: https://www.codingninjas.com/codestudio/problems/sort-0-1-2_631055?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0
+*/
+
+import java.util.* ;
+import java.io.*; 
+public class Solution 
+{
+    public static void sort012(int[] arr)
+    {
+        Arrays.sort(arr);
+    }
+}
