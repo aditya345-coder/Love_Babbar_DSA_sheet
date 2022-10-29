bf1f231f0c1be82abe28aac06acc04633b12c827
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2022-10-29 22:32:41 +0530

    Create 05_Move_All_Negative_Numbers_To_Beginning_And_Positive_To_End.java

---
+++ 01_Arrays/05_Move_All_Negative_Numbers_To_Beginning_And_Positive_To_End.java
@@ -0,0 +1,12 @@
+/*
+Link: https://www.codingninjas.com/codestudio/problems/move-all-negative-numbers-to-beginning-and-positive-to-end_1112620?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0
+*/
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static ArrayList<Integer> separateNegativeAndPositive(ArrayList<Integer> nums) {
+        Collections.sort(nums);
+        return nums;
+    }
+}
