c138fbca0d17201b9eb4eb041cfbe345ee588a34
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2022-10-29 22:47:15 +0530

    Update 05_Move_All_Negative_Numbers_To_Beginning_And_Positive_To_End.java
    Added method 2

--- 01_Arrays/05_Move_All_Negative_Numbers_To_Beginning_And_Positive_To_End.java
+++ 01_Arrays/05_Move_All_Negative_Numbers_To_Beginning_And_Positive_To_End.java
@@ -6,7 +6,38 @@ import java.util.* ;
 import java.io.*; 
 public class Solution {
     public static ArrayList<Integer> separateNegativeAndPositive(ArrayList<Integer> nums) {
+        ArrayList arr = new ArrayList<>();
+        for (int i=0; i<nums.size();i++)
+        {
+            if(nums.get(i)<0)
+            {
+                arr.add(nums.get(i));
+            }
+        }
+        
+        for (int j=0; j<nums.size(); j++)
+        {
+            if(nums.get(j)==0)
+            {
+                arr.add(nums.get(j));
+            }
+            if (nums.get(j)>0)
+            {
+                arr.add(nums.get(j));
+            }
+        }
+        return arr;
+    }
+}
+
+// Method 2 
+/*
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static ArrayList<Integer> separateNegativeAndPositive(ArrayList<Integer> nums) {
         Collections.sort(nums);
         return nums;
     }
 }
+*/
