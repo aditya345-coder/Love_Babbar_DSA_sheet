f2ec6c1550cc003864844ea86d36e17ed5db3a25
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2022-10-31 23:43:56 +0530

    Update 06_Find_similarities_between_two_arrays.java

--- 01_Arrays/06_Find_similarities_between_two_arrays.java
+++ 01_Arrays/06_Find_similarities_between_two_arrays.java
@@ -9,23 +9,27 @@ public class Solution {
 	public static ArrayList<Integer> findSimilarity(ArrayList<Integer> arr1, ArrayList<Integer> arr2, int n, int m) {
            ArrayList <Integer> arr3 = new ArrayList<>();
            ArrayList <Integer> res = new ArrayList<>();
-            for (int i=0; i<n; i++)
+           
+	    // Inserting values of arr1 into new array arr3
+	    for (int i=0; i<n; i++)
             {
                 arr3.add(arr1.get(i));
             }
+	
             for(int i=0; i<m; i++)
             {
-                boolean check=arr3.contains(arr2.get(i));
+		// checking if value to be inserted in arr3 from arr2 already exist?
+                boolean check=arr3.contains(arr2.get(i)); 
                 if (check)
                 {
                     continue;
                 }
-                arr3.add(arr2.get(i));
+                arr3.add(arr2.get(i)); // if the value to be inserted in arr3 from arr2 does not exist in arr3 then it will insert this new value into arr3
             }
-            int count = arr3.size();
-            int union = m+n - count;
-            res.add(union);
-            res.add(count);
+            int count = arr3.size(); // calculting the size of arr3 
+            int union = m+n - count; // finding union {n(A⋃B) = n(A) + n(B) - n(A⋂B)} ( here, count referce to intersection)
+            res.add(union); // inserting variable union in res array
+            res.add(count);  // inserting variable count  in res array ( here, count referce to intersection)
             return res;
 	}
 }
