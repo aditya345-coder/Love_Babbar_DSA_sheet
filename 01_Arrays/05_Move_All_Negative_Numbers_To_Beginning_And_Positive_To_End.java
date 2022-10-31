50ffe85ba21871bf8d63415174e777c74e9f66e0
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2022-10-31 23:57:45 +0530

    Update 05_Move_All_Negative_Numbers_To_Beginning_And_Positive_To_End.java

--- 01_Arrays/05_Move_All_Negative_Numbers_To_Beginning_And_Positive_To_End.java
+++ 01_Arrays/05_Move_All_Negative_Numbers_To_Beginning_And_Positive_To_End.java
@@ -6,7 +6,9 @@ import java.util.* ;
 import java.io.*; 
 public class Solution {
     public static ArrayList<Integer> separateNegativeAndPositive(ArrayList<Integer> nums) {
-        ArrayList arr = new ArrayList<>();
+        // creating new array
+        ArrayList arr = new ArrayList<>(); 
+        // Adding values (less than 0) i.e negative values from nums to arr
         for (int i=0; i<nums.size();i++)
         {
             if(nums.get(i)<0)
@@ -14,16 +16,17 @@ public class Solution {
                 arr.add(nums.get(i));
             }
         }
-        
+        // After adding all the negative values in the arr 
         for (int j=0; j<nums.size(); j++)
         {
-            if(nums.get(j)==0)
+            if(nums.get(j)==0) // checking if the element of array nums is 0 or not
             {
-                arr.add(nums.get(j));
+                arr.add(nums.get(j)); // if it is 0 add the number to arr 
             }
+            // checking if the element of array nums is greter than 0 (positive number)
             if (nums.get(j)>0)
             {
-                arr.add(nums.get(j));
+                arr.add(nums.get(j)); // if it is positive number add the number to arr 
             }
         }
         return arr;
@@ -36,7 +39,7 @@ import java.util.* ;
 import java.io.*; 
 public class Solution {
     public static ArrayList<Integer> separateNegativeAndPositive(ArrayList<Integer> nums) {
-        Collections.sort(nums);
+        Collections.sort(nums); // sorting the array
         return nums;
     }
 }
